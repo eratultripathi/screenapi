48ca44ac9adcf6271d4b3050029f2a8e069ded8f
eratultripathi <117887146+eratultripathi@users.noreply.github.com>
2023-08-02 15:18:05 +0530

    Update screens.js

--- models/screens.js
+++ models/screens.js
@@ -1,1 +1,14 @@
+import mongoose from 'mongoose';
+
+
+const screenSchema= new mongoose.Schema({
+    usersId: { type: String, required: true },
+    type: { type: String, required: true },
+    image: { type: String, required: true },   
+},
+{timestamps: true}
+);
+
+const Screen= mongoose.model("Screen", screenSchema);
+export default Screen;
 
