92c028360fca96a52a3cb80f3aec5c288907a41b
eratultripathi <117887146+eratultripathi@users.noreply.github.com>
2023-08-02 15:18:57 +0530

    Update screen.js

--- routes/screen.js
+++ routes/screen.js
@@ -1,1 +1,11 @@
+import express from "express";
+import { getScreens,createScreen } from "../controllers/screen.js";
 
+const router=express.Router();
+
+
+router.route('/get').get(getScreens);
+router.route('/create').post(createScreen);
+
+
+export default router;
