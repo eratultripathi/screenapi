10c6362d2bdf9f6d79862cd2449a62f224386723
eratultripathi <117887146+eratultripathi@users.noreply.github.com>
2023-08-07 18:36:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,51 +1,66 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
 
-import express from 'express'
-import cors from 'cors'
-import mongoose from 'mongoose'
+import clientProfileRoutes from "./routes/clientprofile.js";
+import employeeProfileRoutes from "./routes/employeeprofile.js";
+import projecteRoutes from "./routes/project.js";
+import credentialRoutes from "./routes/ProjectCredential.js";
+import taskRoutes from "./routes/ProjectTask.js";
+import screenRoutes from "./routes/screenshot.js";
+import employeeprojecttaskRoutes from "./routes/employeeprojecttask.js";
 
-
-
-// routers
-
-import screenRoutes from './routes/screen.js'
-
-
-
-
-const app = express()
-app.use(cors())
-app.use(express.json({ limit: '30mb', extended: true }))
-app.use(express.urlencoded({ limit: '30mb', extended: true }))
-
-
-/// routes
-
-app.use('/api/screen', screenRoutes)
-
-
-
-
-
-// db connection
-mongoose.connect('mongodb+srv://atul:ermechcoder@cluster0.fq4j3um.mongodb.net/Database?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log('Connected to DB')
-    })
-    .catch(err => {
-        console.log(err)
-    })
-
-    app.get('/', (req, res) => {
-        res.send('server is running')
-    })
-
-const PORT=5011
-app.listen(PORT, (req,res) => {
-    console.log(`Server started on port ${PORT}`)
-})
+/* CONFIGURATION */
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 
 
 
 
 
 
+
+app.use("/api/engineer",employeeProfileRoutes);
+
+// app.get("/api/screenshort", async (req, res) => {
+//   const tasks = await  User.find()
+//   res.send(tasks)
+// })
+
+app.use("/api/client",clientProfileRoutes);
+
+app.use("/api/project",projecteRoutes);
+app.use("/api/project",taskRoutes)
+app.use("/api/project",credentialRoutes)
+app.use("/api/project",employeeprojecttaskRoutes)
+app.use("/api",screenRoutes)
+
+
+
+/* Database */
+mongoose.set( 'strictQuery', false);
+const PORT = process.env.PORT || 9000;
+// mongoose.set('strictQuery', true
+// )
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+ 
+  })
+  .catch((error) => console.log(`${error} did not connect`));
