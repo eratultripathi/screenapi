68d2d1ab42bc402d0a8ed7b01d6f49aea6c34cc3
eratultripathi <117887146+eratultripathi@users.noreply.github.com>
2023-08-07 18:37:19 +0530

    Delete clientprofile.js

--- routes/clientprofile.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import {postClientProfile,getClientProfile } from "../controllers/clientprofile.controller.js";
-
-const router = express.Router();
-
-router.post("/profile",postClientProfile);
-router.get("/profile",getClientProfile);
-
-export default router;
